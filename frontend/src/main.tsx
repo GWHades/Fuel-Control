ba2bb4ec1f1d2ac3b7e50ae7181aa270baef726e
GWHades <79339518+GWHades@users.noreply.github.com>
2026-01-12 20:06:56 -0300

    Update main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,19 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import { App } from './App'
-import './styles.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import './styles.css';
 
-import { getToken } from './services/authStore'
-import { setAuthToken } from './services/api'
+import { getToken } from './services/authStore';
+import { setAuthToken } from './services/api';
 
-const token = getToken()
-if (token) setAuthToken(token)
+const token = getToken();
+if (token) {
+  setAuthToken(token);
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
