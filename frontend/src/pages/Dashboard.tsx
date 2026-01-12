5a175a268896509640d37c05f604def36c0e6cdb
GWHades <79339518+GWHades@users.noreply.github.com>
2026-01-12 19:06:35 -0300

    Change Dashboard export to default export

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { api } from '../services/api';
 
-export function Dashboard() {
+export default function Dashboard() {
   const [data, setData] = useState<any>(null);
   const [loading, setLoading] = useState(true);
 
